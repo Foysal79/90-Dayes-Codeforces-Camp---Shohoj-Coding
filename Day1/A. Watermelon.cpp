921207f2c404ad982727a46372e5bc01fdffdc88
Foysal79 <onthor312001@gmail.com>
2026-05-14 20:44:58 +0600

    A. Watermelon solve

--- Day1/A. Watermelon.cpp
+++ Day1/A. Watermelon.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int main() 
 {
+   int n;
+   cin >> n;
 
+   if(n % 2 == 0 && n > 2)
+   {
+    cout << "YES" << endl;
+   }
+   else {
+    cout << "NO" << endl;
+   }
 
 
     return 0;
