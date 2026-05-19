7c719ce7c872befe9e350385bf59e9655c220bb2
Foysal79 <onthor312001@gmail.com>
2026-05-19 23:33:49 +0600

    486A - Calculating Function solve

--- Day6/A. Calculating Function.cpp
+++ Day6/A. Calculating Function.cpp
@@ -4,17 +4,11 @@ int main()
 {
     long long int n, c = 0;
     cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        if (i % 2 == 0)
-        {
-            c += i;
-        }
-        else
-        {
-            c -= i;
-        }
+    if(n % 2 == 0) {
+        cout << n/2 << endl;
     }
-    cout << c << endl;
+    else {
+        cout << -(n+1) / 2 << endl;
+    }
     return 0;
 }
