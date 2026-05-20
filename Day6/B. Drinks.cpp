57e8097ef53b3883b78726d63117d76c5c786018
Foysal79 <onthor312001@gmail.com>
2026-05-20 09:21:18 +0600

    B. Drinks solve

--- Day6/B. Drinks.cpp
+++ Day6/B. Drinks.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main() 
 {
-    
+    double n, sum = 0, total;
+
+    cin >> n;
+    for(int i = 0; i < n; i++) { 
+
+        double x; cin >> x;
+        sum += x;
+    }
+    total = sum / n;
+    cout << fixed << setprecision(12) << total << endl;
     return 0;
 }
