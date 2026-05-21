f86b29f849bf6695d69b3efe01f30e1cade09cf0
Foysal79 <onthor312001@gmail.com>
2026-05-22 01:14:32 +0600

    148A - Insomnia cure solve

---
+++ Day8/A. Insomnia cure.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+    int c = 0;
+    for (int i = 1; i <= d; i++)
+    {
+        if (i % k == 0)
+        {
+            c += 1;
+        }
+        else if (i % l == 0)
+        {
+            c += 1;
+        }
+        else if (i % m == 0)
+        {
+            c += 1;
+        }
+        else if (i % n == 0)
+        {
+            c += 1;
+        }
+    }
+    cout << c << endl;
+    return 0;
+}
