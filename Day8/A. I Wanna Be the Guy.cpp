0ab3e1d5a36afa7f9eff0e0b49499e230d8af298
Foysal79 <onthor312001@gmail.com>
2026-05-22 10:31:26 +0600

    469A - I Wanna Be the Guy solve

--- Day8/A. I Wanna Be the Guy.cpp
+++ Day8/A. I Wanna Be the Guy.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 int main() 
 {
-    int n, c; cin >> n;
-    
+    int n, c = 0, x, y; 
+    int ar[10000];
+    cin >> n;
+ 
+    cin >> x;
+
+    for(int i = 0; i < x; i++) {
+        cin >> ar[i];
+    }
+
+    cin >> y;
+
+    for(int i = x; i < x+y; i++) {
+        cin >> ar[i];
+    }
+    sort(ar, ar +(x+y));
+    for(int i = 0; i < x + y; i++) {
+        if(ar[i] != ar[i+1]) {
+            c++;
+        }
+    }
+    if(c == n) {
+        cout << "I become the guy." << endl;
+    }
+   else {
+    cout << "Oh, my keyboard!" << endl;
+   }
     return 0;
 }
