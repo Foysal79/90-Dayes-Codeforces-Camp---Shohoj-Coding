621ac83e788c383e4792b5cdd0c43d37135eff7f
Foysal79 <onthor312001@gmail.com>
2026-05-24 01:06:26 +0600

    1352A - Sum of Round Numbers solve

--- Day9/A. Sum of Round Numbers.cpp
+++ Day9/A. Sum of Round Numbers.cpp
@@ -1,9 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{  
+{
+    int t;
+    cin >> t;
 
-    
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        vector<string> v;
+        int len = s.size();
+        for (int i = 0; i < len; i++)
+        {
+            if (s[i] != '0')
+            {
+                string num = "";
+                num += s[i];
+
+                for (int j = i + 1; j < len; j++)
+                {
+                    num += "0";
+                }
+
+                v.push_back(num);
+            }
+        }
+        cout << v.size() << endl;
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
