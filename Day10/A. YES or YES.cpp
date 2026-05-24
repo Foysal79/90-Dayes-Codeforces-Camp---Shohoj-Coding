4e99448314e11a77933c6d25f2cde10807dd2dd3
Foysal79 <onthor312001@gmail.com>
2026-05-24 12:39:00 +0600

    1703A - YES or YES? solve

---
+++ Day10/A. YES or YES.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+    string s; cin >> s;
+
+    for(int i = 0; i < 3; i++) {
+
+        if(s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] += 32;
+        }
+    }
+
+    if(s == "yes") {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    }
+
+    return 0;
+}
