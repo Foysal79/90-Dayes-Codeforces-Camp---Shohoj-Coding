fbb2bbd2970c3a624333a47838690e842e76e3f5
Foysal79 <onthor312001@gmail.com>
2026-06-15 11:04:43 +0600

    1669A - Division? solve

--- Day12/A. Division.cpp
+++ Day12/A. Division.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main() 
 {
-    int n, t; cin >> n; 
-    while(n--) {
-        cin >> t; 
-        if(t <= 1399) cout << "Division 4" << endl;
-        else if(1599 <= t) cout << "Division 3" << endl;
-        else if()
+    int t; cin >> t;
+
+    while(t--) {
+        int n; cin >> n;
+        if(n >= 1900) cout << "Division 1" << endl;
+        else if(n >= 1600 &&  n <= 1899) cout << "Division 2" << endl;
+        else if(n >= 1400 && n <= 1599) cout << "Division 3" << endl;
+        else cout << "Division 4" << endl;
     }
-    
     return 0;
 }
