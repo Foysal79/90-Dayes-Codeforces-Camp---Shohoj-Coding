8cd45c03e5dfd366afc47288d64c8f70f22ad55b
Foysal79 <onthor312001@gmail.com>
2026-06-15 12:58:00 +0600

    1999A - A+B Again? solve

---
+++ Day13/A. A+B Again.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t; cin >> t; 
+
+    while(t--) {
+        string s; cin >> s;
+        int x = (s[0] - '0') + (s[1] - '0');
+        cout << x << endl;
+    }
+    return 0;
+}
