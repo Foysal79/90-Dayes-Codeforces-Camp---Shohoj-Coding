c198e8001a060b3bb8fa689623f918d55de28260
Foysal79 <onthor312001@gmail.com>
2026-06-15 12:35:50 +0600

    1676A - Lucky? solve

--- Day12/A. Lucky.cpp
+++ Day12/A. Lucky.cpp
@@ -6,8 +6,16 @@ int main()
 
    while(t--) {
     string s; cin >> s;
+    int f = 0, se = 0;
+     f = ( s[0] - '0' ) + ( s[1] - '0' ) + ( s[2] - '0' );
+     se = ( s[3] - '0' ) + ( s[4] - '0' ) + ( s[5] - '0' ); 
+     if(f == se) {
+       cout << "YES" << endl;
+     } 
+     else {
 
-    
+        cout << "NO" << endl;
+     }
    }
 
     return 0;
