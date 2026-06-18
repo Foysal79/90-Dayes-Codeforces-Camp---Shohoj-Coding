558f323a93d0a8bfd89fd7d12d7167ce3d2d8fc8
Foysal79 <onthor312001@gmail.com>
2026-06-18 06:27:30 +0600

    1512A - Spy Detected! solve

---
+++ Day14/A. Yet Another Two Integers Problem.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t; cin >> t;
+
+    while(t--) {
+        long long int a, b, def, c = 0;
+        cin >> a >> b;
+        cout << ( abs(a-b) + 9 ) / 10 << endl; 
+    }
+    return 0;
+}
